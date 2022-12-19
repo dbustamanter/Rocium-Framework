6719739b9419f6778421f2ca50186acb0e753dbc
Diego Bustamante <inc.diego.bustamante@gmail.com>
2022-12-19 06:15:43 -0300

    configuración multiples navegadores clase driverManager

--- src/test/java/configFramework/driverConfig/driverManager.java
+++ src/test/java/configFramework/driverConfig/driverManager.java
@@ -4,14 +4,28 @@ import com.epam.healenium.SelfHealingDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class driverManager {
     private static SelfHealingDriver driver;
+    private static WebDriver delegate;
 
-    public static void setUpDriver(String URL){
-        WebDriverManager.chromedriver().setup();
-        WebDriver delegate = new ChromeDriver();
-        driver = SelfHealingDriver.create(delegate);
+    public static void setUpDriver(Navegador nav, String URL){
+        switch (nav){
+            case Chrome:
+                WebDriverManager.chromedriver().setup();
+                delegate = new ChromeDriver();
+                driver = SelfHealingDriver.create(delegate);
+            case Firefox:
+                WebDriverManager.firefoxdriver().setup();
+                delegate = new FirefoxDriver();
+                driver = SelfHealingDriver.create(delegate);
+            case Edge:
+                WebDriverManager.edgedriver().setup();
+                delegate = new EdgeDriver();
+                driver = SelfHealingDriver.create(delegate);
+        }
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.get(URL);
